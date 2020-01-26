00ff5183e8625fad60f08ea1353a8ddccfa698c3
pedro resende <pedroresd@gmail.com>
2020-01-26 15:26:05 -0300

    tela do sistema

--- sistema_v1.py
+++ sistema_v1.py
@@ -4,11 +4,18 @@ import time;
 import datetime
 
 root = Tk()
-root.geometry("1250x650+0+0")
-root.title("Projeto Auto Escola")
-root.configure(background = "yellow")
 
-Tops = frame(root, width=1350, height=100, bd=9,relief ="raise")
-Tops.pack(side=left)
+root.geometry("1250x650+50+50")
+root.title("Projeto Auto Escola")
+root.configure(background = "grey")
+
+Tops = Frame (root, width=1350, height=100, bd=9,relief ="raise")
+Tops.pack(side=TOP)
+
+Fleft1 = Frame (root, width=900, height=650, bd=8,relief ="raise")
+Fleft1.pack(side=LEFT)
+
+Fright1 = Frame (root, width=400, height=650, bd=9,relief ="raise")
+Fright1.pack(side=RIGHT)
 
 root.mainloop()
