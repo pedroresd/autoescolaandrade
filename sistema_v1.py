c8c82d36726b1d6ef4f77649fcc721f6a76aaaba
pedro resende <pedroresd@gmail.com>
2020-01-26 14:47:59 -0300

    first commit

---
+++ sistema_v1.py
@@ -0,0 +1,14 @@
+from tkinter import*
+import random
+import time;
+import datetime
+
+root = Tk()
+root.geometry("1250x650+0+0")
+root.title("Projeto Auto Escola")
+root.configure(background = "yellow")
+
+Tops = frame(root, width=1350, height=100, bd=9,relief ="raise")
+Tops.pack(side=TOP)
+
+root.mainloop()
